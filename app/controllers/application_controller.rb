56f4b107f443e44eef7fef434d2548b0bb07bcd8
TaylanPostalci <tay.postal@gmail.com>
2022-12-18 17:34:31 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
+  
+
+private
+
+  def not_found(error)
+    render json: {error:"#{error.model} not found"} , status: :not_found
+  end
+
+  def invalid(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
 end
